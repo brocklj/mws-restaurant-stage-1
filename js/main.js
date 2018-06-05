59d6761203f479de47703f36fc7a663ee5670d9a
Jakub <brockljakub@gmail.com>
2018-06-05 22:33:10 +0200

    fix: registering service worker

--- js/main.js
+++ js/main.js
@@ -25,29 +25,6 @@ registerServiceWorker = () => {
     if (!navigator.serviceWorker.controller) {
       return;
     }
-
-    if (reg.waiting) {
-      indexController._updateReady(reg.waiting);
-      return;
-    }
-
-    if (reg.installing) {
-      indexController._trackInstalling(reg.installing);
-      return;
-    }
-
-    reg.addEventListener('updatefound', function() {
-      indexController._trackInstalling(reg.installing);
-    });
-  });
-
-  // Ensure refresh is only called once.
-  // This works around a bug in "force update on reload".
-  var refreshing;
-  navigator.serviceWorker.addEventListener('controllerchange', function() {
-    if (refreshing) return;
-    window.location.reload();
-    refreshing = true;
   });
 }
 
