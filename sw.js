9cc1539ca3b9e7c1bca1fe42bd66ffe234261c3e
Jakub <brockljakub@gmail.com>
2018-07-06 00:51:27 +0200

    feat: saving data into IndexedDB

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 var contentCache = 'restaurant-stage1-v2';
 var imageCache = 'restaurant-stage1-imgs';
-
+const DATABASE_URL = 'http://localhost:1337';
 var allCaches = [
     contentCache,
     imageCache
@@ -39,7 +39,6 @@ self.addEventListener('activate', function(e){
 
 self.addEventListener('fetch', function(e) {
     let url = new URL(e.request.url);
-
     if (url.origin === location.origin) {
       if (url.pathname === '/') {
         e.respondWith(caches.match('/'));
@@ -54,10 +53,16 @@ self.addEventListener('fetch', function(e) {
         return;
       }
     }
+    if(url.origin === DATABASE_URL){
+        var res = getJsonResponse(e.request);
+        console.log(res);
+        e.respondWith(res);
+        return;
+    }
     e.respondWith(
       caches.match(e.request).then(function(response) {
         return response || fetch(e.request).catch(function(err) {
-                console.log(err);       // fallback mechanism           
+                console.log(err);       // fallback mechanism
             });
       })
     );
@@ -65,7 +70,7 @@ self.addEventListener('fetch', function(e) {
 
   function getImage(request) {
     var storageUrl = request.url.replace(/-\d+px\.jpg$/, '');
-  
+
     return caches.open(imageCache).then(function(cache) {
       return cache.match(storageUrl).then(function(response) {
         if (response) return response;
@@ -76,3 +81,43 @@ self.addEventListener('fetch', function(e) {
       });
     });
   }
+
+  var idbRequest = self.indexedDB.open('RESTAURANTS_DB', 2);
+  idbRequest.onupgradeneeded = function(event){
+      var db = event.target.result;
+      var listStore = db.createObjectStore('restaurants', {keyPath: "url"});      
+  }
+
+  idbRequest.onsuccess = function() {
+    var db = idbRequest.result;
+  }
+
+  function getJsonResponse(request) {
+      var url = new URL(request.url);      
+        return fetch(request).then(function(res){        
+            if(res.ok){
+                saveToIDb(res.clone());
+                return res;       
+            }
+        }).catch(function(err){
+            return respondFromIDb(request);
+        });
+  }
+
+  function respondFromIDb(request) {   
+        var db = idbRequest.result;
+        var tx = db.transaction("restaurants", "readwrite");
+        var store = tx.objectStore("restaurants");
+        var data = store.get(request.url);    
+        return data;
+  }
+
+
+  function saveToIDb(response) {      
+    response.json().then(function(json){            
+            var db = idbRequest.result;
+            var tx = db.transaction("restaurants", "readwrite");
+            var store = tx.objectStore("restaurants");
+            store.put({url: response.url, data: json});
+    });
+}
