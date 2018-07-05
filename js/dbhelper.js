c0e8aed19b43bd4cc6df3ab7e8c6184b5c1c37f9
Jakub <brockljakub@gmail.com>
2018-07-05 09:35:06 +0200

    feat: add usage of fetch into fetchRestaurants

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,27 +8,23 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    const port = 1337 // Change this to your server port
+    return `http://localhost:${port}`;
   }
 
   /**
    * Fetch all restaurants.
    */
   static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) { // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
-        callback(null, restaurants);
-      } else { // Oops!. Got an error from server.
+    let req = new Request(DBHelper.DATABASE_URL + '/restaurants/');  
+    fetch(req).then((res)=>{
+      if(res.ok){
+        res.json().then((restaurants) => callback(null,restaurants));  
+      } else {
         const error = (`Request failed. Returned status of ${xhr.status}`);
         callback(error, null);
-      }
-    };
-    xhr.send();
+      }     
+    });  
   }
 
   /**
@@ -42,7 +38,7 @@ class DBHelper {
       } else {
         const restaurant = restaurants.find(r => r.id == id);
         if (restaurant) { // Got the restaurant
-          callback(null, restaurant);
+          callback(null, restaurant)
         } else { // Restaurant does not exist in the database
           callback('Restaurant does not exist', null);
         }
